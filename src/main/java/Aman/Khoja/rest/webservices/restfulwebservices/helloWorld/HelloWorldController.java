f6be9670691b1b24f06cc1b546251aa625890f20
Aman Khoja <aak171730@utdallas.edu>
2022-11-04 12:08:17 -0500

    added PathVariable mapping

--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -2,6 +2,7 @@ package Aman.Khoja.rest.webservices.restfulwebservices.helloWorld;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //RestAPI
@@ -18,5 +19,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWordPathVariable(@PathVariable String name){
+        return new HelloWorldBean("This is from path variable : HELLO WORLD " + name);
     }
 }
