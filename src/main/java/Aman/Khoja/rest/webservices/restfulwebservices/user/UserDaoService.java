d110b4388bc97c1b2382d42426da30a9a790c5bc
Aman Khoja <aak171730@utdallas.edu>
2022-11-04 12:55:19 -0500

    added functionality to find by ID

--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -20,6 +21,12 @@ public class UserDaoService {
 
     public List<User> findAll(){
         return users;
+    }
+
+    public User findById(int id){
+
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 
 
--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,7 @@ package Aman.Khoja.rest.webservices.restfulwebservices.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,10 @@ public class UserResource {
     @GetMapping(path = "/users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retriveUserbyId(@PathVariable int id){
+        return service.findById(id);
     }
 }
