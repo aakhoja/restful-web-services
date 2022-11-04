f917a30f16fc313f4f966aecd41d1c5cec1942f7
Aman Khoja <aak171730@utdallas.edu>
2022-11-04 00:28:44 -0500

    update getMapping injection & use bean for JSON

--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,8 +1,7 @@
 package Aman.Khoja.rest.webservices.restfulwebservices.helloWorld;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //RestAPI
@@ -11,8 +10,13 @@ public class HelloWorldController {
 
     //  URL - http://localhost:8080/hello-world
 
-    @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
