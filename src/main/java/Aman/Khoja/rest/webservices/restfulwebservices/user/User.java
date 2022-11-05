e5d6b0e64ccb4b7cbae1878794afee11fc844709
Aman Khoja <aak171730@utdallas.edu>
2022-11-05 14:00:13 -0500

    added a message parameter for error handling

--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/User.java
@@ -9,10 +9,10 @@ public class User {
 
     private Integer id;
 
-    @Size(min = 2)
+    @Size(min = 2,message = "Name should have at-least 2 characters.")
     private String name;
 
-    @Past
+    @Past(message = "The birth year cannot be in the future")
     private LocalDate birthday;
 
     public User() {
