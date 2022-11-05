f88c95597f97f6864538da463e450426c2e9a9f6
Aman Khoja <aak171730@utdallas.edu>
2022-11-05 11:44:39 -0500

    added Delete functions on Users

--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/user/UserResource.java
@@ -43,4 +43,9 @@ public class UserResource {
                 .toUri();
         return ResponseEntity.created(location).build();
     }
+
+    @DeleteMapping(path = "/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        service.deleteById(id);
+    }
 }
