d4ec92e5b512c122915e4aabe3ebaa19671ef525
Aman Khoja <aak171730@utdallas.edu>
2022-11-09 15:08:58 -0600

    implemented i18n message source properties

--- src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/Aman/Khoja/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,15 +1,25 @@
 package Aman.Khoja.rest.webservices.restfulwebservices.helloWorld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //RestAPI
 @RestController
 public class HelloWorldController {
 
     //  URL - http://localhost:8080/hello-world
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -24,5 +34,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWordPathVariable(@PathVariable String name){
         return new HelloWorldBean("This is from path variable : HELLO WORLD " + name);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+        //return "Hello World";
     }
 }
